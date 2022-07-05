3ab7d7a0910acc31c624d2d2da846d8a1110771e
jA71 <isjavier77@gmail.com>
2022-07-05 10:30:04 -0500

    Se crea funcion de suma de 3 numeros del 1er apartado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[]args){
-        System.out.println("HELLO THERE");
+    public static void main(String[] args) {
+        sumanum(1, 2, 3);
+    }
+    // Primera parte:
+    // Crear una función con tres parámetros que sean números que se suman entre sí.
+    // Llamar a la función en el main y darle valores.
+
+    public static void sumanum(int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c;
+        System.out.println("-----------");
+        System.out.println("EL RESULTADO DEL 1ER APARTADO ES: ");
+        System.out.println(resultado);
+        System.out.println("-----------");
     }
 }
